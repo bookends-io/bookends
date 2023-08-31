ba212fd83bde8aae974bed8acfb71c079015c006
Sterling Long <sterling.long@lztek.io>
2023-08-31 13:18:37 -0600

    Local file service updates

--- bookends/README.md
+++ bookends/README.md
@@ -1,7 +1,14 @@
-# bookends
+# Bookends
 
-A new Flutter project.
+A project that allows asking questions before and after an activity or event.
+The questions before 
 
 ### Commands
 
-`dart run build_runner build --delete-conflicting-outputs`
+- To run the project: `flutter run`
+- To rebuild generated files: `dart run build_runner build --delete-conflicting-outputs`
+
+
+Our Intention
+Sterling - Pull data from the backend and display it to the user, start working on responses
+Amir - Polish up the getters for the backend (Move up and down the chain)
--- bookends/lib/services/local_file_service/local_file_service.dart
+++ bookends/lib/services/local_file_service/local_file_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:bookends/services/local_file_service/i_local_file_service.dart';
+import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:universal_io/io.dart';
 
@@ -9,11 +10,18 @@ class LocalFileService implements ILocalFileService {
 
   @override
   Future<void> init() async {
+    if (kIsWeb) {
+      return;
+    }
     _directory = await getApplicationDocumentsDirectory();
   }
 
   @override
   Future<List<String>> getAllFileNames() async {
+    if (kIsWeb) {
+      return [];
+    }
+
     final files = _directory.listSync();
     final List<String> fileNames = files
         .map(
@@ -25,6 +33,10 @@ class LocalFileService implements ILocalFileService {
 
   @override
   Future<List<Map<String, dynamic>>> readAll() async {
+    if (kIsWeb) {
+      return [];
+    }
+
     List<Map<String, dynamic>> jsonList = [];
     final files = _directory.listSync();
 
@@ -42,6 +54,10 @@ class LocalFileService implements ILocalFileService {
 
   @override
   Future<Map<String, dynamic>> readFile(String fileName) async {
+    if (kIsWeb) {
+      return {};
+    }
+
     final file = File('${_directory.path}/$fileName');
     String content = await file.readAsString();
     Map<String, dynamic> jsonContent = jsonDecode(content);
@@ -50,6 +66,10 @@ class LocalFileService implements ILocalFileService {
 
   @override
   Future<void> write(String fileName, Map<String, dynamic> data) async {
+    if (kIsWeb) {
+      return;
+    }
+
     final file = File('${_directory.path}/$fileName');
     String jsonStr = jsonEncode(data);
     await file.writeAsString(jsonStr);
@@ -57,12 +77,20 @@ class LocalFileService implements ILocalFileService {
 
   @override
   Future<void> delete(String fileName) async {
+    if (kIsWeb) {
+      return;
+    }
+
     final file = File('${_directory.path}/$fileName');
     await file.delete();
   }
 
   @override
   Future<void> deleteAll() async {
+    if (kIsWeb) {
+      return;
+    }
+
     final files = _directory.listSync();
     for (var file in files) {
       if (file is File) {
