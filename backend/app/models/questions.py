b6c19756272b7d968f89f1814b4b27e3459c3b03
AmirDapl <Amir@daplcorp.com>
2023-08-31 14:18:41 -0500

    Update questions.py

--- backend/app/models/questions.py
+++ backend/app/models/questions.py
@@ -2,12 +2,13 @@ from pydantic import BaseModel, Field
 from typing import List, Optional
 from uuid import uuid4
 
+# Note to Amir, 
 class Question(BaseModel):
     id: str = Field(default_factory=lambda: str(uuid4()))
     text: str
     description: Optional[str] = None
     type: str
-    reliesOn: Optional[List['Question']] = None
+    reliesOn: Optional[str]= None
 
     class Config:
         arbitrary_types_allowed = True
